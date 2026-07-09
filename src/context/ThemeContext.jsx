be2f09777b400289dd324e08c56d8f9d98c827a5
AryanPatel-25 <your-github-email@gmail.com>
2026-07-09 20:14:43 +0530

    fix vercel react routing

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,8 +1,8 @@
 
+import { createContext } from "react";
+import { useState } from "react";
 
-
-export const AuthContext = createContext();
-
+export const ThemeContext = createContext();
 
 export function ThemeProvider({children}){
 
