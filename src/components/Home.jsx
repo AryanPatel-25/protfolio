ff857ef37292480f05e446940635ca98259ea80d
AryanPatel-25 <your-github-email@gmail.com>
2026-07-10 20:54:45 +0530

    fix vercel react routing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,8 +14,11 @@ function Home(){
                 <h1>Hi, I'm Aryan Patel</h1>
 
    <h2>Computer Engineering Student</h2>
+   <h3></h3> 
 
-   <p>Frontend Developer | React Learner</p>
+   <p>I enjoy building modern, responsive web
+applications and continuously exploring
+new technologies in Web Development and AI. Frontend Developer | React Learner</p>
 
 
                 <button>
